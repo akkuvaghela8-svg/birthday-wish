07c14e8ca2a74b714e8ff345656f55ea632933a1
akkuvaghela8-svg <akkuvaghela8@gmail.com>
2026-01-04 22:48:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openGift() {
+    document.getElementById("first").classList.add("hide");
+    document.getElementById("second").classList.remove("hide");
+
+    document.getElementById("bgMusic").play();
+}
+ function celebrate() {
+    window.location.href = "final.html";
+}
+
+
